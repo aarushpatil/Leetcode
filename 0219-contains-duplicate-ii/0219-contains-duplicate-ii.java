b71b19f56674de7bcf31cf8cd9300c2aab37f55f
aarushpatil <73201384+aarushpatil@users.noreply.github.com>
2022-10-22 12:26:45 -0400

    Time: 2376 ms (5.01%), Space: 70.9 MB (82.79%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,16 +1,35 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        //check k numbers ahead for duplicate  -  make sure not to fail out of bounds edge case
+        
+        //trash solution
+        //O(N)^2
+        // for(int i = 0; i < nums.length; i++){
+        //     int count = 0;
+        //     for(int j = i+1; j < nums.length && count < k; j++){
+        //         if(nums[i] == nums[j]){ // fix this
+        //           return true;  
+        //         }
+        //         count++;
+        //     }            
+        // }
+        // return false;
         
         
-        for(int i = 0; i < nums.length; i++){
-            int count = 0;
-            for(int j = i+1; j < nums.length && count < k; j++){
-                if(nums[i] == nums[j]){ // fix this
-                  return true;  
+        //better solution 
+        //time complexity O(n) * k,
+        //check for worst case
+        
+        
+        for(int i = k; i >= 1; i--){ // need this to decrement k until 1
+            int l = 0; 
+            int r = i;
+            while(r < nums.length){
+                if(nums[l] == nums[r]){
+                    return true;
                 }
-                count++;
-            }            
+                l++;
+                r++;
+            }
         }
         return false;
     }
