0777631f979b4474db7992b323d0092fd496f00a
aarushpatil <73201384+aarushpatil@users.noreply.github.com>
2022-10-24 15:12:21 -0400

    Time: 67 ms (17.84%), Space: 78.2 MB (66.15%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,6 +1,43 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
         
+        
+        if(k <= 0){
+            return false;
+        }
+        
+        if(k == 1){
+           for(int i = 1; i < nums.length; i++){
+               if(nums[i-1] == nums[i]){
+                  return true; 
+               }
+           } 
+            return false;
+        }
+        
+        int l = 0;
+        int r = 0;
+        //retry
+        HashSet<Integer> set = new HashSet<>();
+        while(r < k+1 && r < nums.length){
+            if(set.contains(nums[r])){
+                return true;                
+            }
+            set.add(nums[r]);
+            r++;
+        }
+        while(r < nums.length){
+            set.remove(nums[l]);
+            if(set.contains(nums[r])){
+                return true;                
+            }
+            set.add(nums[r]);
+            r++;
+            l++;
+        }
+        return false;
+        
+        
         //trash solution
         //O(N)^2
         // for(int i = 0; i < nums.length; i++){
@@ -36,33 +73,33 @@ class Solution {
         
         //best hashset solution
         
-        if(k <= 0){
-            return false;
-        }
+//         if(k <= 0){
+//             return false;
+//         }
         
-        if(k == 1){
-           for(int i = 1; i < nums.length; i++){
-               if(nums[i-1] == nums[i]){
-                  return true; 
-               }
-           } 
-            return false;
-        }
+//         if(k == 1){
+//            for(int i = 1; i < nums.length; i++){
+//                if(nums[i-1] == nums[i]){
+//                   return true; 
+//                }
+//            } 
+//             return false;
+//         }
         
-        HashSet<Integer> set = new HashSet<>();
+//         HashSet<Integer> set = new HashSet<>();
         
-        for(int i = 0; i < k+1 && i < nums.length; i++){
-            if(!set.add(nums[i])){
-                return true;
-            }
-        }
+//         for(int i = 0; i < k+1 && i < nums.length; i++){
+//             if(!set.add(nums[i])){
+//                 return true;
+//             }
+//         }
         
-        for(int i = set.size(); i < nums.length; i++){
-            set.remove(nums[i-k-1]);
-            if(!set.add(nums[i])){
-                return true;
-            }
-        }
-        return false;
+//         for(int i = set.size(); i < nums.length; i++){
+//             set.remove(nums[i-k-1]);
+//             if(!set.add(nums[i])){
+//                 return true;
+//             }
+//         }
+//         return false;
     }
 }
