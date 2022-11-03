dbaaaa09545d6d0c5e5c2e02f5d895a3710fd147
aarushpatil <73201384+aarushpatil@users.noreply.github.com>
2022-11-03 11:52:46 -0400

    Time: 18 ms (96.66%), Space: 53.3 MB (87.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,41 +1,104 @@
 class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
+       //Nov 3rd
         
-        
-        if(k <= 0){
+        if(k == 0){
             return false;
         }
         
-        if(k == 1){
-           for(int i = 1; i < nums.length; i++){
-               if(nums[i-1] == nums[i]){
-                  return true; 
-               }
-           } 
-            return false;
-        }
-        
-        int l = 0;
-        int r = 0;
-        //retry
+        //loop k times
+        //setup / open window
+        //start left at 0
+        //check if right is in the hashset
+        //add right to hashset
         HashSet<Integer> set = new HashSet<>();
-        while(r < k+1 && r < nums.length){
-            if(set.contains(nums[r])){
-                return true;                
+        for(int i = 0; i < k + 1 && i < nums.length; i++){
+            if(set.contains(nums[i])){
+                return true;
             }
-            set.add(nums[r]);
-            r++;
-        }
-        while(r < nums.length){
-            set.remove(nums[l]);
-            if(set.contains(nums[r])){
-                return true;                
+            set.add(nums[i]);
+        }        
+        
+        //start looping until right side < length;
+    //every iteration of loop remove leftmost from hashset and check if new right value is bread
+        int right = k+1;
+        while(right < nums.length){
+            set.remove(nums[right - k - 1]);
+            if(set.contains(nums[right])){
+                return true;
             }
-            set.add(nums[r]);
-            r++;
-            l++;
+            set.add(nums[right]);
+            right++;
         }
         return false;
+       
+        
+       
+        
+        
+  
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         if(k <= 0){
+//             return false;
+//         }
+        
+//         if(k == 1){
+//            for(int i = 1; i < nums.length; i++){
+//                if(nums[i-1] == nums[i]){
+//                   return true; 
+//                }
+//            } 
+//             return false;
+//         }
+        
+//         int l = 0;
+//         int r = 0;
+//         //retry
+//         HashSet<Integer> set = new HashSet<>();
+//         while(r < k+1 && r < nums.length){
+//             if(set.contains(nums[r])){
+//                 return true;                
+//             }
+//             set.add(nums[r]);
+//             r++;
+//         }
+//         while(r < nums.length){
+//             set.remove(nums[l]);
+//             if(set.contains(nums[r])){
+//                 return true;                
+//             }
+//             set.add(nums[r]);
+//             r++;
+//             l++;
+//         }
+//         return false;
         
         
         //trash solution
