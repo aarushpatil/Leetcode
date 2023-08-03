883f69f0f9ec152c7cba6ebe539c97ab3688041c
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2023-08-03 13:09:20 -0400

    Time: 276 ms (36.93%), Space: 62.7 MB (11.65%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -7,28 +7,56 @@ class Solution {
         }
 
 
-        //brute force:
-        Arrays.sort(nums);
+        //brute force: O(nlogn) time
+        // Arrays.sort(nums);
 
-        int counter = 1;
-        int max = 1;
-        for(int i = 0; i < nums.length-1; i++)
+        // int counter = 1;
+        // int max = 1;
+        // for(int i = 0; i < nums.length-1; i++)
+        // {
+        //     System.out.print(nums[i] + ", ");
+        //     if(nums[i] + 1 == nums[i+1])
+        //     {
+        //         counter++;
+        //         max = Math.max(max, counter);
+        //     }
+        //     else if(nums[i] != nums[i+1])
+        //     {
+        //         counter = 1;
+        //     }
+        // }
+        // return max;
+
+        
+
+
+        //Good solution, O(n) time.
+        //Utilizes set
+        
+        HashSet<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++)
         {
-            System.out.print(nums[i] + ", ");
-            if(nums[i] + 1 == nums[i+1])
-            {
-                
-                counter++;
-                max = Math.max(max, counter);
-            }
-            else if(nums[i] != nums[i+1])
-            {
-                counter = 1;
-            }
+            set.add(nums[i]);
         }
 
-
-        return max;
+        int max = 0;
+        int count = 0;
+        for(int i = 0; i < nums.length; i++)
+        {
+            if(!set.contains(nums[i] - 1))
+            {
+                int temp = nums[i];
+                while(set.contains(temp+1))
+                {
+                    temp++;
+                    count++;
+                }
+                max = Math.max(max, count);
+                count = 0;
+            }
+        }
+        return max+1;
+        
 
     }
 }
