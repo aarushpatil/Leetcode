67edccc374f6e3ad642a1aacfca5eb195e817104
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2023-08-08 21:24:40 -0400

    Time: 0 ms (100%), Space: 41 MB (63.76%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,67 +3,63 @@ class Solution {
 
         if(nums.length == 1)
         {
-            return (nums[0] == target) ? 0:-1;
+            return nums[0] == target ? 0 : -1;
         }
-        //find pivot point using bin search.
-		int left = 0;
-		int right = nums.length - 1;
-		int pivot = -1;
-		if(nums[0] < nums[nums.length-1])
-		{
-			pivot = 0;
-		}
-		while(pivot == -1)
-		{
-			int mid = (left + right)/2;
-            if(nums[mid] == target)
-			{
-				return mid;
-			}
-			if(nums[mid] > nums[mid + 1])
-			{
-				pivot = mid;
-				break;
-			}
-			else
-			{
-				if(nums[left] < nums[mid])
-				{
-					left = mid + 1;
-				}
-				else
-				{
-					right = mid;
-				}
-			}
-		}
-		
-		left = 0;
-		right = pivot;
-		return Math.max(binSearchHelper(left, right, target, nums), binSearchHelper(pivot + 1, nums.length - 1, target, nums));
-    }
 
-    private static int binSearchHelper(int left, int right, int target, int[] nums)
-	{
-		
-		//binary search
-		while(left <= right)
-		{
-			int mid = (left + right)/2;
-			if(target == nums[mid])
-			{
-				return mid;
-			}
-			else if(target > nums[mid])
-			{
-				left = mid + 1;
-			}
-			else
-			{
-				right = mid - 1;
-			}
-		}
-		
-		return -1;
-	}
+        int left = 0;
+        int right = nums.length - 1;
+
+        int pivot = -1;
+        if(nums[left] < nums[right])
+        {
+            pivot = 0;
+        }
+        while(pivot == -1)
+        {
+            int mid = (left + right)/2;
+            if(nums[mid] > nums[mid + 1])
+            {
+                pivot = mid;
+                break;
+            }
+
+            else if(nums[mid] > nums[left])
+            {
+                left = mid + 1;
+            }
+                
+            else
+            {
+                right = mid;
+            }
+        }
+        // return pivot;
+        int res = binSearch(nums, target, 0, pivot);
+        if(res != -1)
+        {
+            return res;
+        }
+        return binSearch(nums, target, pivot+1, nums.length - 1);
+
+    }
+    private static int binSearch(int[] nums, int target, int left, int right)
+    {
+        while(left <= right)
+        {
+            int mid = (left + right) / 2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid] > target)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+        return -1;
+    }
 }
