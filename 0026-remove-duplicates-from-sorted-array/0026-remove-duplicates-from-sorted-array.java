a49aee2ddc47825d044e7a0ceb93b336bced0625
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2023-09-01 10:17:34 -0400

    Time: 75 ms (5.05%), Space: 46 MB (5.72%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,67 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int l = 0;
+        int r = 0;
+        int count = 0;
+
+        while(r < nums.length - 1)
+        {
+            if(l == r)
+            {
+                r++;
+            }
+            else if(nums[l] != nums[r])
+            {
+                l++;
+            }
+            else if(nums[r + 1] != nums[l])
+            {
+                //delete nums[l]
+                nums[l] = -101;
+                count++;
+                l++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+        
+
+        for(int i = 0; i < nums.length; i++)
+        {
+            System.out.print(nums[i] + ", ");
+        }
+        System.out.println("\n\n" + l + ", " + r);
+
+        //remove the last few repeating numbers
+        if(nums[l] == nums[r])
+        {
+            while(l < nums.length - 1)
+            {
+                nums[l] = -101;
+                count++;
+                l++;
+            }
+        }
+
+        //shift all the values over to the left
+        l = 0;
+        r = 0;
+        while(r < nums.length)
+        {
+            if(nums[r] != -101)
+            {
+                nums[l] = nums[r];
+                l++;
+                r++;
+            }
+            else
+            {
+                r++;
+            }
+        }
+
+        return nums.length - count;
+    }
+}
