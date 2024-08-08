531f6f2b4a513256e582d765c5c5be3282a544e1
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-08-07 21:35:16 -0400

    Time: 1 ms (99.53%), Space: 56.6 MB (87.73%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,57 +1,17 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-// //brute force approach 
-//         int maxSum = nums[0];
-//         for(int i = 0; i < nums.length; i++){
-//             if(nums[i] > maxSum){
-//                 maxSum = nums[i];
-//             }
-//         }
-//         for(int left = 0; left < nums.length; left++){
-//             int currSum = nums[left];
-//             for(int right = left+1; right < nums.length; right++){                
-//                 //manage currSum
-//                 currSum += nums[right];
-//                 if(currSum > maxSum){
-//                     maxSum = currSum;
-//                 }
-//             }
-            
-//         }
-//         return maxSum;
-        
-        int sum = Integer.MIN_VALUE;
-        int prevSum = -20001;
-        for(int i = 0; i < nums.length; i++){
-            prevSum = Math.max(prevSum + nums[i], nums[i]);
-            if(prevSum > sum){
-                sum = prevSum;
-            }
+      int curSum = 0;
+      int n = nums.length;
+      int maxSum = Integer.MIN_VALUE;
+      for (int i = 0; i < n; i++) {
+        if (curSum < 0) {
+          curSum = 0;
         }
-        return sum;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//         //Kadane's algorithm
-//         int max = Integer.MIN_VALUE;
-//         int subMax = -10^5;
-//         for(int i = 0; i < nums.length; i++){
-//             subMax = Math.max(nums[i], nums[i] + subMax);
-//             if(max < subMax){
-//                 max = subMax;
-//             }
-//         }
-//         return max;
+        curSum += nums[i];
+        if (curSum > maxSum) {
+          maxSum = curSum;
+        }
+      }
+      return maxSum;
     }
 }
