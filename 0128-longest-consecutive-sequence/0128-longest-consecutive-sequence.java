37d7ab19781d029d575b274176fe20132fef48fa
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-08-14 20:55:26 -0400

    Time: 1093 ms (18.93%), Space: 66.5 MB (47.68%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,86 +1,19 @@
 class Solution {
     public int longestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
 
-        //Good solution, O(n) time.
-        //Utilizes set
+        for (int num: nums) set.add(num);
+        int maxCount = 0;
 
-        //for [] edge case
-        if(nums.length == 0)
-        {
-            return 0;
-        }
-
-        HashSet<Integer> set = new HashSet<Integer>();
-        for(int i = 0; i < nums.length; i++)
-        {
-            set.add(nums[i]);
-        }
-
-        int max = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++)
-        {
-            if(!set.contains(nums[i] - 1))
-            {
-                int counter = 0;
-                int temp = nums[i];
-                //potential start of a consecutive elements sequence
-                while(set.contains(temp + 1))
-                {
-                    temp++;
-                    counter++;
-                }
-                max = Math.max(max, counter);
+        for (int num: nums) {
+            if (set.contains(num - 1)) continue;
+            int count = 1;
+            while (set.contains(num + count)) {
+                count++;
             }
+
+            maxCount = Math.max(maxCount, count);
         }
-        return max + 1;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // if(nums.length == 0)
-        // {
-        //     return 0;
-        // }
-
-
-        //brute force: O(nlogn) time
-        // Arrays.sort(nums);
-
-        // int counter = 1;
-        // int max = 1;
-        // for(int i = 0; i < nums.length-1; i++)
-        // {
-        //     System.out.print(nums[i] + ", ");
-        //     if(nums[i] + 1 == nums[i+1])
-        //     {
-        //         counter++;
-        //         max = Math.max(max, counter);
-        //     }
-        //     else if(nums[i] != nums[i+1])
-        //     {
-        //         counter = 1;
-        //     }
-        // }
-        // return max;
-
+        return maxCount;
     }
 }
