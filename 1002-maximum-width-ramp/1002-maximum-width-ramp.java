1a75c0a746d9cd3f299d8bc1645d3acdc81264c9
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-10-12 09:54:43 -0400

    Time: 11 ms (78.98%), Space: 55.2 MB (58.27%) - LeetHub

--- 1002-maximum-width-ramp/1002-maximum-width-ramp.java
+++ 1002-maximum-width-ramp/1002-maximum-width-ramp.java
@@ -1,34 +1,60 @@
 class Solution {
     public int maxWidthRamp(int[] nums) {
         
-        int[] right = new int[nums.length];
-        right[nums.length-1] = nums[nums.length - 1];
-        for(int i = nums.length - 2; i >= 0; i--)
+        // int[] right = new int[nums.length];
+        // right[nums.length-1] = nums[nums.length - 1];
+        // for(int i = nums.length - 2; i >= 0; i--)
+        // {
+        //     right[i] = Math.max(right[i + 1], nums[i]);
+        // }
+        // System.out.println(Arrays.toString(right));
+
+
+        // int left = 0;
+        // int r = 0;
+
+        // int maxW = 0;
+        // while(r < nums.length)
+        // {
+        //     if(left >= r || nums[left] <= right[r])
+        //     {
+        //         //yk that theres a bigger option
+        //         r++;
+        //     }
+        //     else
+        //     {
+        //         left++;
+        //     }
+        //     maxW = Math.max(r-left-1, maxW);
+        // }
+
+        // return maxW;
+
+
+        List<Integer> list = new LinkedList<>();
+
+        list.addFirst(0);
+
+        for(int i = 1; i < nums.length; i++)
         {
-            right[i] = Math.max(right[i + 1], nums[i]);
+            if(nums[list.get(0)] > nums[i])
+            {
+                list.addFirst(i);
+            }
         }
-        System.out.println(Arrays.toString(right));
-
-
-        int left = 0;
-        int r = 0;
 
         int maxW = 0;
-        while(r < nums.length)
+        for(int i = nums.length - 1; i >= 0 && list.size() > 0; i--)
         {
-            if(left >= r || nums[left] <= right[r])
+            while(list.size() > 0 && nums[i] >= nums[list.get(0)]) //make this a while
             {
-                //yk that theres a bigger option
-                r++;
+                maxW = Math.max(maxW, i - list.get(0));
+                list.removeFirst();
             }
-            else
-            {
-                left++;
-            }
-            maxW = Math.max(r-left-1, maxW);
         }
 
         return maxW;
+
     }
 }
 
