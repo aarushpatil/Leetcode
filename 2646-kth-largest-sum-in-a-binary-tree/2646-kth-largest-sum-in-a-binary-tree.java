171c5cb90078bc0c5740d9afa864f23d0648382f
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-10-24 01:28:04 -0400

    Time: 63 ms (5.26%), Space: 66.4 MB (20.28%) - LeetHub

---
+++ 2646-kth-largest-sum-in-a-binary-tree/2646-kth-largest-sum-in-a-binary-tree.java
@@ -0,0 +1,77 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public long kthLargestLevelSum(TreeNode root, int k) {
+        Queue<Integer> levelQ = new LinkedList<Integer>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        long[] levelSum = new long[100000];
+
+        //key is level index
+        //value is sum
+
+        //bfs over tree
+        q.add(root);
+        levelQ.add(0);
+
+        while(!q.isEmpty())
+        {
+            TreeNode currNode = q.poll();
+            int currLevel = levelQ.poll();
+            if(currNode == null) continue;
+
+            levelSum[currLevel] += currNode.val;
+
+            levelQ.add(currLevel + 1);
+            q.add(currNode.left);
+
+            levelQ.add(currLevel + 1);
+            q.add(currNode.right);
+        }
+
+        // System.out.println(Arrays.toString(levelSum));
+
+        ArrayList<Long> nums = new ArrayList<>();
+        for(long n : levelSum)
+        {
+            if(n == 0)
+            {
+                break;
+            }
+            nums.add(n);
+        }
+
+        System.out.println(nums);
+        if(k > nums.size())
+        {
+            return -1;
+        }
+
+        PriorityQueue<Long> priQueue = new PriorityQueue<>(Collections.reverseOrder());
+        priQueue.addAll(nums);
+
+        for(int i = 0; i < k-1; i++)
+        {
+            priQueue.poll();
+        }
+
+        return priQueue.poll();
+
+
+        // Arrays.sort(levelSum);
+    }
+}
