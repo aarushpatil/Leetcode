54bc9e5c86396e1e482c82a91b9bac8377697861
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-11-23 00:42:02 -0500

    Time: 3 ms (48.02%), Space: 45.5 MB (73.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,22 +1,11 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        boolean[] arr = new boolean[nums.length];
-        arr[0] = true;
-        for(int i = 0; i < nums.length - 1; i++)
+        int max = 0;
+        for(int i = 0; i < nums.length && i <= max; i++)
         {
-            if(arr[i])
-            {
-                for(int k = 0; k < nums[i]; k++)
-                {
-                    if(i + k + 1 < arr.length)
-                    {
-                        arr[i + k + 1] = true;
-                    }
-                }
-            }
-            // System.out.println("\n\n");
+            max = Math.max(max, nums[i] + i);
         }
-        // System.out.println(Arrays.toString(arr));
-        return arr[nums.length - 1];
+
+        return max >= nums.length - 1;
     }
 }
