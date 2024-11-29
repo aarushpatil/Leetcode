e1184099007f7003bc3e463c1cde93275e3aa534
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-11-30 05:10:21 +0530

    Time: 771 ms (5%), Space: 45.7 MB (5.18%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,15 +1,12 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         HashMap<Long, Integer> freqMap = new HashMap<>();
-
         for(int i = 0; i < nums.length; i++)
         {
             freqMap.put((long)nums[i], freqMap.getOrDefault((long)nums[i], 0) + 1);
         }
-
         List<List<Integer>> sol = new ArrayList<>();
         HashSet<String> seen = new HashSet<>();
-
         for(int i = 0; i < nums.length; i++)
         {
             for(int j = i + 1; j < nums.length; j++)
@@ -23,22 +20,18 @@ class Solution {
                     if(freqMap.containsKey(miss))
                     {
                         int count = 1;
-
                         if(miss == nums[i])
                             count++;
                         if(miss == nums[j])
                             count++;
                         if(miss == nums[k])
                             count++;
-
                         if(freqMap.get(miss) >= count)
                         {
                             List<Integer> list = new ArrayList<>();
                             list.add(nums[i]);
                             list.add(nums[j]);
                             list.add(nums[k]);
-                            // System.out.println(missing);
-                            // System.out.println((int)missing);
                             list.add((int)miss);
                             Collections.sort(list);
                             String str = list.toString();
