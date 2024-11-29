d64ded300c55ad3182d77372adc6405187e846c7
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-11-30 04:31:55 +0530

    Time: 220 ms (36.96%), Space: 56.9 MB (83.42%) - LeetHub

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.java
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.java
@@ -2,12 +2,8 @@ class Solution {
     int[][] mat;
     boolean[][] visited;
     public int minimumTime(int[][] grid) {
-
         if(grid[0][1] > 1 && grid[1][0] > 1)
-        {
             return -1;
-        }
-        
         visited = new boolean[grid.length][grid[0].length];
         mat = grid;
         PriorityQueue<int[]> q = new PriorityQueue<>((a, b) -> a[0] - b[0]);
@@ -19,17 +15,11 @@ class Solution {
             int x = curr[1];
             int y = curr[2];
             if(visited[x][y]) continue;
-            
             visited[x][y] = true;
-
-            System.out.println(time  + " : " + x + " " + y);
-
             if(x == grid.length - 1 && y == grid[0].length - 1)
             {
                 return time;
             }
-
-
             if(isValid(x + 1, y))
             {
                 int newTime = grid[x+1][y];
@@ -42,14 +32,12 @@ class Solution {
                 int t = Math.max(time + 1, (time - newTime) % 2 == 0 ? newTime + 1 : newTime);
                 q.add(new int[]{t, x, y + 1});
             }
-
             if(isValid(x - 1, y))
             {
                 int newTime = grid[x - 1][y];
                 int t = Math.max(time + 1, (time - newTime) % 2 == 0 ? newTime + 1 : newTime);
                 q.add(new int[]{t, x - 1, y});
             }
-
             if(isValid(x, y - 1))
             {
                 int newTime = grid[x][y - 1];
@@ -57,10 +45,8 @@ class Solution {
                 q.add(new int[]{t, x, y - 1});
             }
         }
-
         return -1;
     }
-
     boolean isValid(int x, int y)
     {
         if(x < mat.length && y < mat[0].length && x >= 0 && y >= 0 && !visited[x][y])
@@ -70,4 +56,3 @@ class Solution {
         return false;
     }
 }
-
