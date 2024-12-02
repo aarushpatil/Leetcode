7ff1a2566dac5d64b09aa4b0ca2d2003e52418d4
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-12-02 06:29:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,69 +1,85 @@
 class Solution {
-    public int[][] merge(int[][] intervals) {
-        //sort intervals by first number
-        Arrays.sort(intervals, (a,b) -> a[0] - b[0]);
+    public int[][] merge(int[][] intervals) { 
+        // case 1: completely inside
+        // s ------------------ e
+        //        s ------- e
+
+        // case 2: normal 
+        //s ------- e
+        //     s ------------ e
+
+        // case 3: boundary connected
+        //s ------- e
+        //          s ------------ e
+
+        // case 3: boundary connected
+        //          s ------------ e
+        //          s ------- e
+
+        // case 4: same
+        //          s ------------ e
+        //          s ------------ e
+
+
+
+        //unmergeable case
+
+        // case 4: 
+        //s ------------ e
+        //                   s ------------ e
+
+        //sort the intervals by start.
+        //if start is inside or equal previous interval's end, then you can definetly merge
+
+        //if start is > end of previous, then cant merge
+
+        List<int[]> list = new LinkedList<>();
+
+        //[] merge [] merge []
+        //[] merge [] no me []
+        //[] no.   [].  no. []  need to add last from prev
+        //[]. no.  [] merge []. need to add last from prev
         
-        //start at 0th interval. Compare ith interval with i+1 interval until i<length-1
-
-        //store current interval
-        int left = 10001;
-        int right = -1;
-        
-        //counts how many intervals
-        int counter = 0;
-
-        for(int i = 0; i < intervals.length - 1; i++) {
-            if(intervals[i][0] == intervals[i+1][0]) {
-                // left = Math.min(left, intervals[i][0]);
-                // right = Math.max(intervals[i][1], intervals[i+1][1]);
-
-                intervals[i+1][0] = Math.min(left, intervals[i][0]);
-                intervals[i+1][1] = Math.max(intervals[i][1], intervals[i+1][1]);
-
-                intervals[i][0] = -1;
-                intervals[i][1] = -1;
-                counter++;
-            }
-            else if(intervals[i][1] > intervals[i+1][1]){
-                // left = Math.min(left, intervals[i][0]);
-                // right = intervals[i][1];
-
-               
-                intervals[i+1][0] = Math.min(left, intervals[i][0]);
-                intervals[i+1][1] = intervals[i][1];
-
-                intervals[i][0] = -1;
-                intervals[i][1] = -1;
-                counter++;
-
-            }
-            else if(intervals[i+1][0] <= intervals[i][1]) {
-                // left = Math.min(left, intervals[i][0]);
-                // right = intervals[i+1][1];
-
-                intervals[i+1][0] = Math.min(left, intervals[i][0]);
-                intervals[i+1][1] = intervals[i+1][1];
-
-                intervals[i][0] = -1;
-                intervals[i][1] = -1;
-                counter++;
-            }
-            else {
-                left = 10001;
-            }
-        }
-
-        //makes return arr from number of combined intervals.
-        int[][] retArr = new int[intervals.length - counter][];
-        int c = 0;
-        for(int i = 0; i < intervals.length; i++)
+        int[] prev = new int[]{intervals[0][0], intervals[0][1]};
+        for(int i = 1; i < intervals.length; i++)
         {
-            if(!(intervals[i][0] == -1)) {
-                retArr[c] = intervals[i];
-                c++;
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+            
+            int prevStart = prev[0];
+            int prevEnd = prev[1];
+
+            if(start > prevEnd)
+            {
+                //cant merge
+                list.add(prev);
+                prev = new int[] {start, end};
+            }
+            else
+            {
+                //merge
+                int beg = prevStart;
+                int en = Math.max(end, prevEnd);
+                prev = new int[] {beg, en};
             }
         }
 
-        return retArr;
+        //add the last one
+        list.add(prev);
+        // int[] last = intervals[intervals.length - 1];
+        // int[] tail = list.get(list.size() - 1);
+        // if(tail[0] == last[0] && tail[1] == last[1])
+        // {
+            
+        // }
+        int[][] out = new int[list.size()][];
+        int c = 0;
+        for(int[] ar : list)
+        {
+            out[c] = ar;
+            c++;
+        }
+
+        return out;
     }
 }
