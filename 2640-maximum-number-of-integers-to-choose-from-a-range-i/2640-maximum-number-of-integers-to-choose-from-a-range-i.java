fe950d2eea985ba3f8ee88e8f622e195f0546281
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-12-06 10:31:59 +0530

    Time: 46 ms (77.72%), Space: 46.2 MB (9.09%) - LeetHub

--- 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -1,39 +1,40 @@
 class Solution {
     public int maxCount(int[] banned, int n, int maxSum) {
+        // Calculate the theoretical maximum `n` that fits in maxSum
         int myN = Math.min(n, getMinN(maxSum));
         int ans = myN;
 
+        // Precompute the sum of the first `myN` natural numbers
         int sumWithMyN = (myN * (myN + 1)) / 2;
-        int bannedSum = maxSum - sumWithMyN;
+        int remainingSum = maxSum - sumWithMyN;
+
+        // Add banned numbers to a set for quick lookups
         HashSet<Integer> bannedSet = new HashSet<>();
-        for(int num : banned)
-        {
+        for (int num : banned) {
             bannedSet.add(num);
         }
 
+        // Adjust for banned numbers in the range [1, myN]
         for (int i = 1; i <= myN; i++) {
             if (bannedSet.contains(i)) {
-                bannedSum += i;
+                remainingSum += i;
                 ans--;
             }
         }
 
-        int currNum = myN + 1; //bannedSum > currNum
-        while(bannedSum >= currNum && currNum <= n)
-        {
-            if(!bannedSet.contains(currNum))
-            {
-                bannedSum -= currNum;
+        // Use remaining sum to consider numbers beyond `myN` that are not banned
+        for (int i = myN + 1; i <= n && remainingSum >= i; i++) {
+            if (!bannedSet.contains(i)) {
+                remainingSum -= i;
                 ans++;
             }
-            currNum++;
         }
+
         return ans;
-        //2+3+4+0+0+7+8
     }
 
-    int getMinN(int maxSum)
-    {
+    // Helper function to compute the maximum n that fits within maxSum
+    private int getMinN(int maxSum) {
         return (int) ((-1 + Math.sqrt(1 + 8L * maxSum)) / 2);
     }
 }
