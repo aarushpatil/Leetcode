238f7ce675c8a2c236c43a4140966db966db8373
Aarush Patil <73201384+aarushpatil@users.noreply.github.com>
2024-12-06 10:24:48 +0530

    Time: 58 ms (14.13%), Space: 46.1 MB (16.36%) - LeetHub

---
+++ 2640-maximum-number-of-integers-to-choose-from-a-range-i/2640-maximum-number-of-integers-to-choose-from-a-range-i.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int maxCount(int[] banned, int n, int maxSum) {
+        int myN = Math.min(n, getMinN(maxSum));
+        // System.out.println(myN);
+        int ans = myN;
+
+        int sumWithMyN = (myN * (myN + 1)) / 2;
+        int bannedSum = maxSum - sumWithMyN;
+        HashSet<Integer> bannedSet = new HashSet<>();
+        for(int num : banned)
+        {
+            bannedSet.add(num);
+        }
+
+        for(int num : bannedSet)
+        {   
+            if(num <= myN)
+            {
+                bannedSum += num;
+                ans--;
+            }
+        }
+
+        // System.out.println(ans);
+        // System.out.println(bannedSum);
+        int currNum = myN + 1; //bannedSum > currNum
+        while(bannedSum >= currNum && currNum <= n)
+        {
+            if(!bannedSet.contains(currNum))
+            {
+                bannedSum -= currNum;
+                ans++;
+            }
+            currNum++;
+        }
+        return ans;
+        //2+3+4+0+0+7+8
+    }
+
+    int getMinN(int maxSum)
+    {
+        double sol = (-1 + Math.sqrt(1 + 8 * maxSum)) / 2;
+
+        return (int) sol;
+    }
+}
